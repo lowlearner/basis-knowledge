4b31e8d6723e25f586f50c9db61a1dbd274cec12
big one <33947536+lowlearner@users.noreply.github.com>
2017-12-27 16:59:30 +0800

    Add files via upload

---
+++ 十进制转化为十六进制.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h> 
+int main()
+{
+	int i;
+	printf("please input decimalism number :\n");
+	scanf ("%d",&i);
+	printf("the hex number is %x",i);
+    system("pause");
+}
